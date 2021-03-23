0e22013bdea893f9332a6e179bc78838b048f495
SergReva <SergReva@users.noreply.github.com>
2021-03-23 22:47:20 +0200

    day 1
    простое домашнее задание

--- js/main.js
+++ js/main.js
@@ -8,3 +8,37 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// cart
+
+const buttonCart = document.querySelector('.button-cart'),
+modalCart = document.querySelector('#modal-cart'),
+modalClose = document.querySelector('.modal-close');
+
+const openModal = () => {
+	modalCart.classList.add('show');
+};
+
+const closeModal = () => {
+	modalCart.classList.remove('show');
+};
+
+buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+// scroll smooth
+
+(function() {
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', (event) => {
+			event.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+
+		});
+	}
+})()
