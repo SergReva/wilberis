d8dd85cf19308f8e0203b8d1a1d309c6656b3d16
SergReva <SergReva@users.noreply.github.com>
2021-04-02 00:26:27 +0300

    server data
    обработка данных на сервере

--- server.php
+++ server.php
@@ -1,3 +1,16 @@
 <?php
 sleep(3);
-echo $_SERVER['REMOTE_ADDR'];
+// echo $_SERVER['REMOTE_ADDR'];
+
+
+$userCart = $_POST['cart'];
+$userName = $_POST['nameCustomer'];
+$userTel = $_POST['phoneCustomer'];
+echo $userCart;
+
+$file = file_get_contents('./db/' . $userName . '__' . $userTel . '.json');
+unset($file);
+
+$taskList[] = array('cart' => $userCart);
+file_put_contents('./db/' . $userName . '__' . $userTel . '.json', $userCart);
+unset($taskList);
