04804c5ae56538c764682eaa614e2c408588fc29
SergReva <SergReva@users.noreply.github.com>
2021-04-04 22:25:54 +0300

    php update

--- server.php
+++ server.php
@@ -6,11 +6,12 @@ sleep(3);
 $userCart = $_POST['cart'];
 $userName = $_POST['nameCustomer'];
 $userTel = $_POST['phoneCustomer'];
+$today = date("mdy.gi");
 echo $userCart;
 
-$file = file_get_contents('./db/' . $userName . '__' . $userTel . '.json');
+$file = file_get_contents('./db/' . $userName . '.' . $userTel . '.' . $today . '.json');
 unset($file);
 
 $taskList[] = array('cart' => $userCart);
-file_put_contents('./db/' . $userName . '__' . $userTel . '.json', $userCart);
+file_put_contents('./db/' . $userName . '__' . $userTel . '__' . $today . '.json', $userCart);
 unset($taskList);
